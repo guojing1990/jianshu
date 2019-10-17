4153d55f6be843bcb647820dbbda4c2867fcbe37
guojing <guojing64745@offcn.com>
2019-10-17 10:44:00 +0800

    完善

--- todolist/src/TodoItem.js
+++ todolist/src/TodoItem.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 class TodoItem extends Component {
     // constructor (props) {
     //     super(props);
     // }
+    // 当父组件的render函数被运行时，它的子组件的render都将被重新运行
     render () {
-        const {item} = this.props;
-        return <div onClick={this.handleClick}>{item}</div>
+        const {item, test} = this.props;
+        return <div onClick={this.handleClick}>{test} - {item}</div>
     }
     handleClick = () => {
         const {deleteItem, index} = this.props;
         deleteItem(index);
     }
+}
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    item: PropTypes.string,
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+TodoItem.defaultProps = {
+    test: 'hhh'
 }
 export default TodoItem;
