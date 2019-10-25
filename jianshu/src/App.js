e695adb1ed4f59b86933a957c32fa5bc0306bbc5
guojing <guojing64745@offcn.com>
2019-10-25 16:42:30 +0800

    专题

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -4,6 +4,9 @@ import {GloablStyledIcon} from './statics/iconfont/iconfont';
 import Header from './common/header';
 import store from './store';
 import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './pages/home';
+import Detail from './pages/detail';
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
         <GloablStyled />
         <GloablStyledIcon />
         <Header />
+        <BrowserRouter>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/detail" exact  component={Detail}></Route>
+        </BrowserRouter>
       </div>
     </Provider>
   );
---
+++ jianshu/src/pages/home/components/Topic.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {
+    TopicWrapper,
+    TopicItem
+} from '../style';
+class Topic extends Component {
+    render () {
+        const {topicList} = this.props
+        return (
+            <TopicWrapper>
+                {
+                    topicList.map((item) => (
+                        <TopicItem key={item.get('id')}>
+                            <img 
+                                alt=""
+                                className="topic-pic" 
+                                src={item.get('imgUrl')}
+                            />
+                            {item.get('title')}
+                        </TopicItem>
+                    ))
+                }
+                
+            </TopicWrapper>
+        )
+    }
+}
+const mapStateToProps = (state) => ({
+    topicList: state.getIn(['home', 'topicList'])
+})
+export default connect(mapStateToProps, null)(Topic);
